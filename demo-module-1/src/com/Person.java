ba72eb34eaa3c5d24708fb29f36a84a9081ea864
1104492973@qq.com <1104492973@qq.com>
2022-05-24 22:18:38 +0800

    hello master

--- demo-module-1/src/com/Person.java
+++ demo-module-1/src/com/Person.java
@@ -4,6 +4,6 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("我是人类");
 
-        System.out.println("hello master");
+        System.out.println("hello Master");
     }
 }
